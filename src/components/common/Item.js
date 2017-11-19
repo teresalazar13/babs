bea107c277c57be0550a53e9b6c3c6e6ba5bdac4
Teresa Margarida Baptista Cabral De Carvalho Salazar <tmsalazar@student.dei.uc.pt>
2017-11-19 11:01:44 +0000

    Adds toggle to show/hide Respond to Request in item.

--- src/components/common/Item.js
+++ src/components/common/Item.js
@@ -1,20 +1,63 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableHighlight } from 'react-native';
 
 class Item extends Component<{}> {
+
+  constructor(props) {
+    super(props);
+    this.state = { hide: true };
+  }
+
+  renderRespond() {
+    const { respondHide, respond } = styles;
+
+    if (this.state.hide == true) {
+      return respondHide;
+    }
+    return {};
+  }
 
   render() {
     return (
       <View style={styles.container}>
-        <View style={{ flexDirection:'row', justifyContent: 'space-between', padding: 5 }}>
-          <View style={{ flexDirection:'column' }}>
-            <Text style={styles.title}>NUGGETS</Text>
-            <Text style={styles.subtitle}>Rua Miguel Torga, n 150</Text>
+        <TouchableHighlight
+          onPress={() => this.setState({ hide: !this.state.hide} )}
+          underlayColor="#f2f2f2"
+        >
+          <View>
+
+            <View style={{ flexDirection:'row', justifyContent: 'space-between', padding: 5 }}>
+              <View style={{ flexDirection:'column' }}>
+                <Text style={styles.title}>NUGGETS</Text>
+                <Text style={styles.subtitle}>Rua Miguel Torga, n 150</Text>
+              </View>
+              <View style={styles.circle}>
+                <Text style={styles.price}>7€</Text>
+              </View>
+            </View>
+
+            <View style={this.renderRespond()}>
+
+              <View style={{ flexDirection:'row', justifyContent: 'space-between', padding: 5, marginTop: 10 }}>
+                <View style={{ flexDirection:'column' }}>
+                  <Text style={styles.title}>Alexandre Santos</Text>
+                  <Text style={styles.subtitle}>914098900</Text>
+                </View>
+                <View style={styles.containerTime}>
+                  <Text style={styles.time}>18:30</Text>
+                </View>
+              </View>
+
+              <View style={styles.containerBottom}>
+                <View style={styles.containerTerminate}>
+                  <Text style={styles.terminate}>R E S P O N D E R</Text>
+                </View>
+              </View>
+
+            </View>
+
           </View>
-          <View style={styles.circle}>
-            <Text style={styles.price}>7€</Text>
-          </View>
-        </View>
+        </TouchableHighlight>
       </View>
     );
   }
@@ -59,7 +102,68 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     alignSelf: 'center',
     fontSize: 12
-  }
+  },
+
+  respondHide: {
+    height: 0,
+    opacity: 0
+  },
+
+  title: {
+    color: 'black',
+    fontFamily: 'arial',
+    fontWeight: 'bold',
+    fontSize: 12
+  },
+
+  subtitle: {
+    color: '#b3b2b3',
+    fontFamily: 'arial',
+    fontWeight: 'bold',
+    paddingTop: 8,
+    fontSize: 10
+  },
+
+  containerTime: {
+    paddingLeft: 15,
+    paddingRight: 15,
+    borderRadius: 10,
+    backgroundColor: '#b3b2b3',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+
+  time: {
+    color: 'white',
+    fontFamily: 'arial',
+    fontWeight: 'bold',
+    alignSelf: 'center',
+    fontSize: 12
+  },
+
+  containerBottom: {
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+
+  containerTerminate: {
+    marginTop: 15,
+    marginBottom: 10,
+    width: 150,
+    padding: 10,
+    borderRadius: 10,
+    backgroundColor: '#b7e885',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+
+  terminate: {
+    color: 'black',
+    fontFamily: 'arial',
+    fontWeight: 'bold',
+    alignSelf: 'center',
+    fontSize: 10
+  },
 });
 
 
